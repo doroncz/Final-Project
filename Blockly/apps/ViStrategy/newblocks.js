0acb5729fdb737d3c0ee8c974b21864759e1f904
doron cz <doroncz@gmail.com>
2013-12-26 17:14:59 +0200

    new slide block

--- Blockly/apps/ViStrategy/newblocks.js
+++ Blockly/apps/ViStrategy/newblocks.js
@@ -26,3 +26,30 @@ Blockly.Python['vis_stock'] = function(block) {
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.Python.ORDER_NONE];
 };
+
+//slide
+Blockly.Blocks['slide'] = {
+  init: function() {
+    this.setHelpUrl('http://www.example.com/');
+    this.appendValueInput("fromDate")
+        .setCheck("String");
+    this.appendValueInput("toDate")
+        .setCheck("String");
+    this.appendValueInput("stock")
+        .setCheck("String");
+    this.setTooltip('');
+  }
+};
+
+Blockly.Python['slide'] = function(block) {
+  var value_fromdate = Blockly.Python.valueToCode(block, 'fromDate', Blockly.Python.ORDER_ATOMIC);
+  var value_todate = Blockly.Python.valueToCode(block, 'toDate', Blockly.Python.ORDER_ATOMIC);
+  var value_stock = Blockly.Python.valueToCode(block, 'stock', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble Python into code variable.
+  
+
+  var code = 'Stock.objects.filter(date__range=('
+   code = code.concat(value_fromdate, ',', value_todate,')).aggregate(Avg(\'base_value\'))');
+  //var code = value_fromdate;
+  return code;
+};
