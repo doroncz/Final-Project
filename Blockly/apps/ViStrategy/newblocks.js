4a0ac9e4fe7932f74b1400c54fdf15c9c3bc75a1
doron cz <doroncz@gmail.com>
2014-01-13 20:58:16 +0200

    update blockly

--- Blockly/apps/ViStrategy/newblocks.js
+++ Blockly/apps/ViStrategy/newblocks.js
@@ -30,13 +30,17 @@ Blockly.Python['vis_stock'] = function(block) {
 //slide
 Blockly.Blocks['slide'] = {
   init: function() {
-    this.setHelpUrl('http://www.example.com/');
+    this.setHelpUrl(Blockly.Msg.slide_HELPURL);
+    this.setColour(240);
     this.appendValueInput("fromDate")
-        .setCheck("String");
+        .setCheck("String")
+        .appendTitle("Sliding Window:   From Date");
     this.appendValueInput("toDate")
-        .setCheck("String");
+        .setCheck("String")
+        .appendTitle("                             To Date");
     this.appendValueInput("stock")
-        .setCheck("String");
+        .setCheck("String")
+        .appendTitle("                             Stock");
     this.setTooltip('');
   }
 };
@@ -46,10 +50,27 @@ Blockly.Python['slide'] = function(block) {
   var value_todate = Blockly.Python.valueToCode(block, 'toDate', Blockly.Python.ORDER_ATOMIC);
   var value_stock = Blockly.Python.valueToCode(block, 'stock', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
-  
-
-  var code = 'Stock.objects.filter(date__range=('
-   code = code.concat(value_fromdate, ',', value_todate,')).aggregate(Avg(\'base_value\'))');
-  //var code = value_fromdate;
+  // var code = 'Stock.objects.filter(date__range=('
+  //  code = code.concat(value_fromdate, ',', value_todate,')).aggregate(Avg(\'base_value\'))');
+var code = 'Stock.objects.filter(stock_name = ' + value_stock + ', date__range=(' + value_fromdate 
+  + ',' + value_todate + ')).aggregate(Avg(\'base_value\'))';
   return code;
+};
+
+Blockly.JavaScript['slide'] = function(block) {
+  /*var value_fromdate = Blockly.JavaScript.valueToCode(block, 'fromDate', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_todate = Blockly.JavaScript.valueToCode(block, 'toDate', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_stock = Blockly.JavaScript.valueToCode(block, 'stock', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'Stock.objects.filter(stockName = ' + value_stock + ' date__range=(' + value_fromdate + ',' + value_todate + ')).aggregate(Avg(\'base_value\'))';
+  // code = code.concat(value_fromdate, ',', value_todate,')).aggregate(Avg(\'base_value\'))');
+ //$.getJSON('/url/to/ajax/view/', {foo: 'bar'}, function(data, jqXHR){// do something with response });
+  // TODO: Change ORDER_NONE to the correct strength.*/
+  window.alert("Please copy the code from Python Tab to the Url: 127.0.0.1:8000/testViStrategyDB/execute_code/");
+ /*$.getJSON('/url/to/ajax/view/', {foo: 'bar'}, function(data, jqXHR){
+    // do something with response
+
+});*/
+ var code='127.0.0.1:8000/testViStrategyDB/execute_code/';
+  return code;//[code, Blockly.JavaScript.ORDER_NONE];
 };
